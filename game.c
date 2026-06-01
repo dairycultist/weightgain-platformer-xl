ec5cadbbbc425b89916bda6965d7c8dc63b4984d
Erik <kazumafan46@gmail.com>
2026-05-31 22:47:26 -0400

    Candy collection and clean up

--- game.c
+++ game.c
@@ -12,7 +12,7 @@ typedef struct {
 
 } CharacterState;
 
-static CharacterState states[] = {
+const static CharacterState states[] = {
 	{ 0, 32, 32, 0.1, 0.3, 3.0, -5.5 },
 	{ 128, 32, 32, 0.05, 0.15, 2.5, -5.5 },
 	{ 256, 48, 48, 0.035, 0.1, 2.0, -4.0 },
@@ -20,14 +20,17 @@ static CharacterState states[] = {
 	{ 640, 96, 48, 0.01, 0.05, 0.5, -1.0 }
 };
 
-static int curr_state;
+static CharacterState curr_state = states[0];
+static int curr_state_i = 0;
 
-static float px, py;
-static float pdx, pdy;
+static float p_x, p_y;
+static float p_dx, p_dy;
+
+static int candy_count;
 
 static int cam_off;
 
-static float animt;
+static float p_animt;
 
 static int facing_left;
 static int grounded;
@@ -35,66 +38,75 @@ static int grounded;
 #define LEVEL_HEIGHT 13
 #define LEVEL_WIDTH (sizeof(level) / sizeof(int) / LEVEL_HEIGHT)
 
+#define IS_COLLIDABLE(i) (i == 1 || i == 2)
+
+static int level_animt;
+
 static int level[] = { // to satisfy spatial locality, level data is stored column-major
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,
-	0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1,
-	0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1,
-	0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1,
+	0, 0, 0, 0, 3, 0, 0, 1, 0, 3, 0, 1, 1,
+	0, 0, 0, 0, 3, 0, 0, 1, 0, 3, 0, 1, 1,
+	0, 0, 0, 0, 3, 0, 0, 1, 0, 3, 0, 1, 1,
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 1, 1,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 1, 1,
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,
+	0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 0, 1, 1,
+	0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 0, 1, 1,
+	0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 0, 1, 1,
+	0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 0, 1, 1,
+	0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 0, 1, 1,
+	0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 0, 1, 1,
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1,
 };
 
+static void get_player_level_aabb(int *start_x, int *start_y, int *end_x, int *end_y) {
+
+	*start_x = (p_x - curr_state.w / 2) / 16;
+	*start_y = (p_y - curr_state.h - 72) / 16;
+
+	if (*start_y < 0)
+		*start_y = 0;
+
+	*end_x = (p_x - 1 + curr_state.w / 2) / 16;
+	*end_y = (p_y - 1 - 72) / 16;
+
+	if (*end_y > LEVEL_HEIGHT - 1)
+		*end_y = LEVEL_HEIGHT - 1;
+}
+
 static int player_is_colliding() {
 
-	CharacterState state = states[curr_state];
-
-	// with left side of screen
-	if (px - state.w / 2 < cam_off)
+	// colliding with left side of screen?
+	if (p_x - curr_state.w / 2 < cam_off)
 		return 1;
 
 	// TEMP until we have proper level-end, gotta stop the player from clipping into the right edge of the level
-	if (px + state.w / 2 - 1 > LEVEL_WIDTH * 16)
+	if (p_x + curr_state.w / 2 - 1 > LEVEL_WIDTH * 16)
 		return 1;
 
-	// with the level
-	int start_x = (px - state.w / 2) / 16;
-	int start_y = (py - state.h - 72) / 16;
+	// colliding with the level?
+	int start_x, start_y, end_x, end_y;
 
-	if (start_y < 0)
-		start_y = 0;
-
-	int end_x = (px - 1 + state.w / 2) / 16;
-	int end_y = (py - 1 - 72) / 16;
-
-	if (end_y > LEVEL_HEIGHT - 1)
-		end_y = LEVEL_HEIGHT - 1;
+	get_player_level_aabb(&start_x, &start_y, &end_x, &end_y);
 
 	for (int x = start_x; x <= end_x; x++) {
 
 		for (int y = start_y; y <= end_y; y++) {
 
-			if (level[y + x * LEVEL_HEIGHT])
+			if (IS_COLLIDABLE(level[y + x * LEVEL_HEIGHT]))
 				return 1;
 		}
 	}
@@ -104,123 +116,137 @@ static int player_is_colliding() {
 
 static void increase_state() {
 
-	curr_state++;
-	
-	if (curr_state == sizeof(states) / sizeof(CharacterState))
-		curr_state = 0;
+	if (curr_state_i == sizeof(states) / sizeof(CharacterState) - 1)
+		return;
 
-	CharacterState state = states[curr_state];
+	curr_state_i++;
+
+	curr_state = states[curr_state_i];
 
 	// prevent clipping into left side of screen
-	if (px - state.w / 2 < cam_off)
-		px = state.w / 2 + cam_off;
+	if (p_x - curr_state.w / 2 < cam_off)
+		p_x = curr_state.w / 2 + cam_off;
 
 	// TEMP until we have proper level-end, gotta stop the player from clipping into the right edge of the level
-	if (px + state.w / 2 - 1 > LEVEL_WIDTH * 16)
-		px = LEVEL_WIDTH * 16 - state.w / 2;
+	if (p_x + curr_state.w / 2 - 1 > LEVEL_WIDTH * 16)
+		p_x = LEVEL_WIDTH * 16 - curr_state.w / 2;
 
 	// prevent clipping into a tile
 	while (player_is_colliding())
-		py--;
+		p_y--;
 }
 
 void game_init() {
 
-	px = 48.0;
-	py = 200.0;
+	p_x = 48.0;
+	p_y = 200.0;
 	
-	set_background(80, 180, 255);
+	set_background(100, 200, 255);
 }
 
 void game_update(const Input *input) {
 
 	// update camera offset
-	if (px + 128 > LEVEL_WIDTH * 16) {
+	if (p_x + 128 > LEVEL_WIDTH * 16) {
 		
 		cam_off = LEVEL_WIDTH * 16 - WIDTH;
 
-	} else if (cam_off < px - 128) {
+	} else if (cam_off < p_x - 128) {
 
-		cam_off = px - 128;
+		cam_off = p_x - 128;
 	}
 
 	// draw level
 	for (int i = 0; i < sizeof(level) / sizeof(int); i++) {
 
-		if (level[i])
-			draw_level(0, (i / LEVEL_HEIGHT) * 16 - cam_off, 72 + (i % LEVEL_HEIGHT) * 16);
+		switch (level[i]) {
+
+			default:
+			case 0:
+				break;
+			
+			case 1: // unbreakable tile
+				draw_level(0, (i / LEVEL_HEIGHT) * 16 - cam_off, 72 + (i % LEVEL_HEIGHT) * 16);
+				break;
+			
+			case 2: // breakable tile
+				draw_level(1, (i / LEVEL_HEIGHT) * 16 - cam_off, 72 + (i % LEVEL_HEIGHT) * 16);
+				break;
+			
+			case 3: // candy
+				draw_level(2 + ((level_animt / 16) % 4), (i / LEVEL_HEIGHT) * 16 - cam_off, 72 + (i % LEVEL_HEIGHT) * 16);
+				break;
+		}
 	}
 
-	draw_text("vivian.5       world 1-1", 2, 2);
+	draw_level(2, 2, 2);
 
-	// TEMP size-changing
-	if (input->action_b_justchanged && input->action_b)
-		increase_state();
-
-	CharacterState state = states[curr_state];
+	char candy_text[] = ".\0\0\0"; // TODO "world 1-1"
+	candy_text[1] = candy_count + '0';
+	draw_text(candy_text, 17, 8);
 
 	// running
 	if (!input->left && !input->right) {
 
-		pdx *= 0.95;
+		p_dx *= 0.95;
 
 	} else if (!grounded || !input->down) {
 
 		if (input->left) {
 			facing_left = 1;
-			pdx -= pdx > 0 ? state.run_deceleration : state.run_acceleration;
-			if (-pdx > state.max_run_speed)
-				pdx = -state.max_run_speed;
+			p_dx -= p_dx > 0 ? curr_state.run_deceleration : curr_state.run_acceleration;
+			if (-p_dx > curr_state.max_run_speed)
+				p_dx = -curr_state.max_run_speed;
 		}
 
 		if (input->right) {
 			facing_left = 0;
-			pdx += pdx < 0 ? state.run_deceleration : state.run_acceleration;
-			if (pdx > state.max_run_speed)
-				pdx = state.max_run_speed;
+			p_dx += p_dx < 0 ? curr_state.run_deceleration : curr_state.run_acceleration;
+			if (p_dx > curr_state.max_run_speed)
+				p_dx = curr_state.max_run_speed;
 		}
 
 	} else {
 		
-		pdx *= 0.95;
+		p_dx *= 0.95;
 	}
 
 	// jump
 	if (grounded && input->action_a_justchanged && input->action_a) {
-		pdy = state.jump_speed;
+		p_dy = curr_state.jump_speed;
 		grounded = 0;
 	}
 
 	// early release jump
-	if (pdy < 0 && input->action_a_justchanged && !input->action_a) {
-		pdy /= 2;
+	if (p_dy < 0 && input->action_a_justchanged && !input->action_a) {
+		p_dy /= 2;
 	}
 
 	// gravity
-	pdy += 0.2;
+	p_dy += 0.2;
 
 	// apply velocity while respecting collision
-	px += pdx;
+	p_x += p_dx;
 
 	if (player_is_colliding()) {
 
 		do {
-			px -= pdx * 0.1;
+			p_x -= p_dx * 0.1;
 		} while (player_is_colliding());
 
-		pdx = 0;
+		p_dx = 0;
 	}
 
-	py += pdy;
+	p_y += p_dy;
 
 	if (player_is_colliding()) {
 
 		do {
-			py -= pdy * 0.1;
+			p_y -= p_dy * 0.1;
 		} while (player_is_colliding());
 
 		grounded = 1;
-		pdy = 1;
+		p_dy = 1;
 
 	} else {
 
@@ -233,41 +259,66 @@ void game_update(const Input *input) {
 	if (input->down) {
 
 		anim_x = 0;
-		anim_y = 3 * state.h;
+		anim_y = 3 * curr_state.h;
 
 	} else if (!grounded) {
 
-		if (pdy > -state.jump_speed / 2.7) {
-			anim_x = 2 * state.w;
-			anim_y = 2 * state.h;
-		} else if (pdy < state.jump_speed / 2.7) {
+		if (p_dy > -curr_state.jump_speed / 2.7) {
+			anim_x = 2 * curr_state.w;
+			anim_y = 2 * curr_state.h;
+		} else if (p_dy < curr_state.jump_speed / 2.7) {
 			anim_x = 0;
-			anim_y = 2 * state.h;
+			anim_y = 2 * curr_state.h;
 		} else {
-			anim_x = state.w;
-			anim_y = 2 * state.h;
+			anim_x = curr_state.w;
+			anim_y = 2 * curr_state.h;
 		}
 		
-	} else if (pdx == 0 || (!input->left && !input->right && pdx < state.run_acceleration && pdx > -state.run_acceleration)) {
+	} else if (p_dx == 0 || (!input->left && !input->right && p_dx < curr_state.run_acceleration && p_dx > -curr_state.run_acceleration)) {
 
 		// TODO idle animation
 		anim_x = 0;
 		anim_y = 0;
-		animt = 0;
+		p_animt = 0;
 
-	} else if (facing_left == pdx > 0) { // turning animation occurs when facing left but moving right (or opposite)
+	} else if (facing_left == p_dx > 0) { // turning animation occurs when facing left but moving right (or opposite)
 	
-		anim_x = state.w;
-		anim_y = 3 * state.h;
-		animt = 0;
+		anim_x = curr_state.w;
+		anim_y = 3 * curr_state.h;
+		p_animt = 0;
 	
 	} else {
 
-		anim_x = (int) animt % 4 * state.w;
-		anim_y = state.h;
+		anim_x = (int) p_animt % 4 * curr_state.w;
+		anim_y = curr_state.h;
 	}
 
-	draw_character(state.w, state.h, anim_x, anim_y + state.ss_off, (int) px - state.w / 2 - cam_off, (int) py - state.h, facing_left);
+	draw_character(curr_state.w, curr_state.h, anim_x, anim_y + curr_state.ss_off, (int) p_x - curr_state.w / 2 - cam_off, (int) p_y - curr_state.h, facing_left);
 
-	animt += (pdx > 0 ? pdx : -pdx) * 0.1;
+	p_animt += (p_dx > 0 ? p_dx : -p_dx) * 0.1;
+
+	level_animt++;
+
+	// collect candy
+	int start_x, start_y, end_x, end_y;
+	
+	get_player_level_aabb(&start_x, &start_y, &end_x, &end_y);
+
+	for (int x = start_x; x <= end_x; x++) {
+
+		for (int y = start_y; y <= end_y; y++) {
+
+			if (level[y + x * LEVEL_HEIGHT] == 3) {
+
+				candy_count++;
+				level[y + x * LEVEL_HEIGHT] = 0;
+
+				if (candy_count == 5) {
+
+					candy_count = 0;
+					increase_state();
+				}
+			}
+		}
+	}
 }
